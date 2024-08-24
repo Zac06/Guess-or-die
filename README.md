61acea503234712c962026301ee0308782d0a710
Zaccaria <zaccaria.francescato@gmail.com>
2024-08-24 19:19:00 +0200

    added base

---
+++ README.md
@@ -0,0 +1,7 @@
+# Guess or DIE
+
+This game, build around my own SDL2 abstraction layer (SDL_base), is a guessing game (take for example the show \"Who wants to be a millionaire?\"), that gives you some... penalties, you could say. 
+
+---
+
+WARNING: save your work before launching the game. Have fun!
---
+++ include/gui_elements/gui_button.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_mixer.h>
+#include<SDL2/SDL_ttf.h>
+#include<SDL2/SDL_image.h>
+
+#include "base_elements/gui_element.hpp"
+
+using namespace std;
+
+class gui_button : public gui_element{
+    public:
+        /// @brief Shortest constructor.
+        /// @param p_tex_normal Texture at a normal state.
+        /// @param p_events Main event manager.
+        /// @param p_coords X and Y coordinates of the texture.
+        gui_button(SDL_Texture* p_tex_normal, SDL_Point p_coords);
+
+        /// @brief Mid-length constructor.
+        /// @param p_tex_normal Texture in a normal state.
+        /// @param p_tex_active Texture in an active state (while cursor is over and mouse pressed)
+        /// @param p_events Main event manager.
+        /// @param p_coords X and Y coordinates os the textures.
+        gui_button(SDL_Texture* p_tex_normal, SDL_Texture* p_tex_active, SDL_Point p_coords);
+
+        /// @brief Full-length construtor.
+        /// @param p_tex_normal Texture in a normal state.
+        /// @param p_tex_hover Texture in a hovered state (when the cursor is over it)
+        /// @param p_tex_active Texture in an active state (while cursor is over and mouse pressed)
+        /// @param p_tex_disabled Texture in a disabled state
+        /// @param p_events Main event manager.
+        /// @param p_coords X and Y coordinates of the texture.
+        gui_button(SDL_Texture* p_tex_normal, SDL_Texture* p_tex_hover, SDL_Texture* p_tex_active, SDL_Texture* p_tex_disabled, SDL_Point p_coords);
+
+        /// @brief Checks for new states to become.
+        void update() override;
+
+    private:
+
+
+};
